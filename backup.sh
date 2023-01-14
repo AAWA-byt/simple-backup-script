0b65bf074f2f61891b48ad388b1d8337469dc479
Aaron Wagner <76434239+AAWA-byt@users.noreply.github.com>
2023-01-14 22:58:30 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TIME=`date +%b-%d-%y`                             # This Command will read the date
-FILENAME=backup-cloudnet-$TIME.tar.gz             # The filename including the date
-SRCDIR=/home/AAWA/CloudNet                        # Source backup folder
-DESDIR=/home/AAWA/Backups/CloudNet-Backups        # Destination of backup file
+FILENAME=backup-$TIME.tar.gz                      # The filename including the date
+SRCDIR=/home/User                                 # Source backup folder
+DESDIR=/root/Backups                              # Destination of backup file
 tar -cpzf $DESDIR/$FILENAME $SRCDIR               # create compressed ZIP folder
